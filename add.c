1fef6650c7a1d6ddad68ab7a74371b40cb6a0822
Aneta <anetapostrozny@gmail.com>
2021-01-04 23:27:30 +0000

    add

---
+++ add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern int main();
+
+struct movies
+{
+      int ID;
+      char title[20];
+      char review[50];
+      double rate;
+} film[70];
+
+int add()
+{
+    FILE *fp;
+    fp=fopen("movies.txt","w+");
+    int i=0,rozmiar;
+    char n,t;
+    do
+    {
+      printf("Podaj tytuł filmu\n:");
+      scanf("%s", *&film[i].title);
+      printf("\nJak ocenisz film w skali od 1 do 10?:\n");
+      scanf("%lf", &film[i].rate);
+      printf("\nDodaj komentarz:\n");
+      scanf("%s", *&film[i].review);
+      film[i].ID=i+1;
+      fwrite(&film[i],sizeof(film),1,fp);
+      i++;
+      rozmiar=i;
+      printf("\nCzy chcesz wpisac do bazy jeszcze jeden rekord? (t=tak)\n");
+      scanf("%s", &t);
+      }while (t=='t');
+
+    fclose(fp);
+    printf("\nAby przejsc do menu wcisnij 1, aby wyjść wciśnij dowolny klawisz\n");
+
+    scanf("%s", &n);
+    fflush(stdin);
+
+    switch(n)
+         {
+         case '1' : main();
+         break;
+         default : exit(1);
+         break;
+         }
+return 0;
+} 
--- main.c
+++ main.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "functions.h"
 
-
-typedef struct movie
-{
-       char title[50];
-       char review[200];
-       double rate;
-} movie;
 
 int main()
 {
-    char n;
-
-printf("Witaj w swojej osobistej Bazie filmów!\n");
-printf("Masz do wyboru nastepujace mozliwosci\n");
-printf("[1] Dodanie filmu do bazy \n[2]Drukowanie bazy danych\n[3]Wyjście");
-do{
-  printf("\nTwoj wybor to?\n");
   char n;
-  scanf("%s", &n);
-  fflush(stdin);
+  printf("Witaj w swojej osobistej Bazie filmów!\n");
+  printf("Masz do wyboru nastepujace mozliwosci\n");
+  printf("[1]Dodanie filmu do bazy \n[2]Drukowanie bazy danych\n[3]Wyjście");
+  do{
+    printf("\nTwoj wybor to?\n");
+    char n;
+    scanf("%s", &n);
+    fflush(stdin);
 
     switch(n)
          {
-         case '1' : //add();
+         case '1' : add();
          break;
          case '2' : //view();
          break;
@@ -33,7 +26,7 @@ do{
          break;
          default : printf("\nNie ma takiej opcji, wybierz jeszcze raz");
          break;
-         }         
+         }
   }while(n!='1'&&n!='2'&&n!='3');
-
+  return 0;
 }
