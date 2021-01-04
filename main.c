16d3244afbe9b7dfa0bf879dc50a9841fa8f5d7f
Aneta <anetapostrozny@gmail.com>
2021-01-04 00:26:14 +0000

    main

--- main.c
+++ main.c
@@ -16,20 +16,22 @@ int main()
 
 printf("Witaj w swojej osobistej Bazie filmów!\n");
 printf("Masz do wyboru nastepujace mozliwosci\n");
-printf("[1] Dodanie filmu do bazy \n[2]Drukowanie bazy danych\n");
+printf("[1] Dodanie filmu do bazy \n[2]Drukowanie bazy danych\n[3]Wyjście");
 do{
-printf("\nTwoj wybor to?\n");
-int n;
-scanf("%d", &n);
-fflush(stdin);
+  printf("\nTwoj wybor to?\n");
+  char n;
+  scanf("%s", &n);
+  fflush(stdin);
 
     switch(n)
          {
-         case 1 : //add();
+         case '1' : //add();
          break;
-         case 2 : //view();
+         case '2' : //view();
          break;
-         default : printf("\n nie ma takiej opcji, wybierz jeszcze raz");
+         case '3' : exit(0);
+         break;
+         default : printf("\nNie ma takiej opcji, wybierz jeszcze raz");
          break;
          }         
   }while(n!='1'&&n!='2'&&n!='3');
